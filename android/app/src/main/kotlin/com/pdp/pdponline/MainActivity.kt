2a7ab2343786d68c97b1b583dc6152ea551e8f48
Khurshid <shamsun.com@gmail.com>
2020-06-22 18:50:18 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/pdp/pdponline/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pdp.pdponline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  static final String id = "detail_page";
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
